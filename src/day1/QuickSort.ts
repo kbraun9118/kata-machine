b6ada3da320ca85f665afea5c10c91b9087447d2
Kurt Braun <kbraun@shelterinsurance.com>
2024-02-27 08:48:34 -0600

    Implemented quick_sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,34 @@
-export default function quick_sort(arr: number[]): void {
+export default function quick_sort(
+    arr: number[],
+    low: number = 0,
+    hi: number = arr.length,
+): void {
+    if (hi <= low) {
+        return;
+    }
+    const p = partition(arr, low, hi);
 
+    quick_sort(arr, low, p);
+    quick_sort(arr, p + 1, hi);
+}
+
+function partition(arr: number[], low: number, hi: number): number {
+    let pivot = arr[hi - 1];
+    let pivot_index = low;
+
+    for (let i = low; i < hi - 1; i++) {
+        if (arr[i] <= pivot) {
+            const tmp = arr[pivot_index];
+            arr[pivot_index] = arr[i];
+            arr[i] = tmp;
+            pivot_index++;
+        }
+    }
+
+    pivot_index++;
+    const tmp = arr[pivot_index];
+    arr[pivot_index] = arr[hi - 1];
+    arr[hi - 1] = tmp;
+
+    return pivot_index;
 }
