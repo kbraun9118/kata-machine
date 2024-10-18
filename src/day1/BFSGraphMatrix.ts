110e41b86fc500f6597ce4c9b55ad50684b36359
Kurt Braun <kbraun9118@gmail.com>
2024-10-18 10:45:42 -0500

    finished a few things

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,43 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const previous: number[] = new Array(graph.length).fill(-1);
+    const seen: boolean[] = new Array(graph.length).fill(false);
 
+    seen[source] = true;
+    const queue = [source];
+
+    while (queue.length !== 0) {
+        const current = queue.shift() as number;
+        if (current === needle) {
+            break;
+        }
+
+        const adjs = graph[current];
+        for (let i = 0; i < adjs.length; i++) {
+            if (adjs[i] === 0 || seen[i]) {
+                continue;
+            }
+
+            previous[i] = current;
+            seen[i] = true;
+            queue.push(i);
+        }
+    }
+
+    if (previous[needle] === -1) {
+        return null;
+    }
+
+    const path = [needle];
+    let current = needle;
+
+    while (previous[current] !== -1) {
+        path.push(previous[current]);
+        current = previous[current];
+    }
+
+    return path.reverse();
 }
--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,80 @@
 export default class MinHeap {
     public length: number;
-
-    
+    data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
     insert(value: number): void {
-
-}
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
     delete(): number {
+        if (this.length === 0) {
+            throw new Error("Could not delete from empty heap");
+        }
 
-}
+        const out = this.data[0];
+        this.length--;
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+
+        const p = this.parent(idx);
+        const parentV = this.data[p];
+        const v = this.data[idx];
+
+        if (parentV > v) {
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+
+    private heapifyDown(idx: number): void {
+        if (idx >= this.length) {
+            return;
+        }
+
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (lIdx >= this.length) {
+            return;
+        }
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lV > rV && v > rV) {
+            this.data[idx] = rV;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+        } else if (rV > lV && v > lV) {
+            this.data[idx] = lV;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+    }
 }
